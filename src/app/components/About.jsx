e0f94da982974f4628775aa35e36e6eddaed006e
Lucky A <105838230+jameslucky007@users.noreply.github.com>
2025-08-13 21:19:29 +0530

    changes

--- src/app/components/About.jsx
+++ src/app/components/About.jsx
@@ -14,7 +14,7 @@ const About = () => {
         >
           <Image
             className="aboutimg"
-            src="/aboutimg.webp"
+            src="https://images.unsplash.com/photo-1754763679553-e7afebe65d44?q=80&w=1171&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
             alt="About Image"
             width={800}
             height={400}
